8752922948bbff7e7f3d77db0c78202b020be148
Natalia <nannimagi@gmail.com>
2025-09-05 10:58:52 +0200

    error bcrypt

--- src/api/controllers/users.js
+++ src/api/controllers/users.js
@@ -44,12 +44,13 @@ const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
-      return res.status(400).json("El usuario o la contraseña son incorrectos");
+      return res.status(400).json("El usuario o la contraseña son incorrectos", error);
     }
 
     const isValidPassword = bcrypt.compareSync(password, user.password);
+      console.log("Comparación:", password, "==>", isValidPassword);
     if (!isValidPassword) {
-      return res.status(400).json("El usuario o la contraseña son incorrectos");
+      return res.status(400).json("El usuario o la contraseña son incorrectos", error);
     }
 
     const token = generateKey(user._id.toString());
